68674c58da67c5086ebcb8a62774d7b6fcd3d7ab
loregamer <162062830+loregamer@users.noreply.github.com>
2025-04-06 22:15:02 -0700

    Update nexus-content-curator.user.js

--- nexus-content-curator.user.js
+++ nexus-content-curator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nexus Mods - Content Curator
 // @namespace    http://tampermonkey.net/
-// @version      1.12.4
+// @version      1.13
 // @description  Adds warning labels to mods and their authors
 // @author       loregamer
 // @match        https://www.nexusmods.com/*
